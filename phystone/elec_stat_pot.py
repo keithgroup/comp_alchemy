9f9afaed527f538ecb269acc2d060f7198c097d7
Chasz Griego <charles.griego18@gmail.com>
2020-01-20 13:04:06 -0500

    edited and formatted elec_stat_pot.py to PEP8

--- phystone/elec_stat_pot.py
+++ phystone/elec_stat_pot.py
@@ -1,85 +1,184 @@
 """
-elec_stat_pot.py
-A Python package for computational alchemy, a tool that allows high-throughput screening of heterogeneous catalysts. 
+Module for manipulating electrostatic potentials printed in VASP OUTCARS.
 
 """
 #!/bin/env python
-from subprocess import Popen, PIPE, STDOUT
-from ase import Atom
-from ase.io import read
 from math import ceil
 
-def grab_esp(slab,outcar):
-    '''Finds electrostatic potentials printed in a VASP OUTCAR (outcar) corresponding to the system described by the POSCAR (poscar).
-Electrostatic potentials are indexed with atom indexes defined with ASE.'''
+def grab_esp(slab, outcar):
+    """
+    Finds electrostatic potentials printed in a VASP OUTCAR (`outcar`) corresponding
+    to the system described by the `slab`.
+
+    This function only works with atoms objects from ASE. Electrostatic potentials are
+    indexed with the ASE atom indexes.
+
+    Parameters
+    ----------
+    slab : An atoms object from ASE.
+
+    outcar : Path to a VASP OUTCAR file of calculation done for slab.
+
+    Returns
+    -------
+    esp : List of electrostatic potentials for each atom in slab.
+    """
+
+    #The following two variables are used to find the chunk of electrostatic
+    #potentials at the end of the OUTCAR. They're printed in a table with size
+    # of rows x 5.
     atoms = float(len(slab))
     rows = int(ceil(atoms/5))
+
+    #A counter used to find the first line of electrostatic potentials after the
+    #string "(electrostatic)" is found in the loop below
     add = 3
-    elec = []
+
+    esp = []
+
     with open(outcar, 'r') as out:
+
         lines = out.readlines()
-    for i,l in enumerate(lines):
-        s = l.split()
-        for j,c in enumerate(s):
-            if c == '(electrostatic)':
-                d = i
-    for k in range(d+add,d+add+rows):
-        for e in lines[k].split()[1::2]:
-            elec.append(float(e))
-    return elec
 
-def espdiff(elec1,elec2,pair):
-    '''Calculating differences in electrostatic potential per atom between systems 1 and 2.
-Diff = esp_2 - esp_1
-Obtain electrostatic potential array with grab_esp().
+    for i, line in enumerate(lines):
 
-First and second arguments are arrays of electrostatic potentials for systems 1 and 2. 
-Third is index pairs between elec1 and elec2
-'''
+        line_split = line.split()
+
+        for chunk in line_split:
+
+            if chunk == '(electrostatic)':
+
+                loc = i
+
+    for k in range(loc + add, loc + add + rows):
+
+        for num in lines[k].split()[1::2]:
+
+            esp.append(float(num))
+
+    return esp
+
+def espdiff(esp_1, esp_2, pairs):
+    """
+    Calculating differences in electrostatic potential per atom between systems 1 and 2.
+
+    Diff = esp_2 - esp_1
+
+    Obtain electrostatic potential array with `grab_esp()`.
+
+    Parameters
+    ----------
+    esp_1 : List of electrostatic potentials for system 1 (see `grab_esp()`).
+
+    esp_2 : List of electrostatic potentials for system 2 (see `grab_esp()`).
+
+    pairs : List of lists that contains pairs of indexes for atoms that match
+        between systems 1 and 2 (see `ads_slab_pairs.pairs()`).
+
+    Returns
+    -------
+    diffs : List of electrostatic potential differences.
+    """
+
     diffs = []
-    for p in pair:
-        diff = elec2[p[1]]-elec1[p[0]]
+
+    for pair in pairs:
+
+        diff = esp_2[pair[1]]-esp_1[pair[0]]
         diffs.append(diff)
+
     return diffs
 
-def remove_duplicate_espdiffs(dexlist,espdiffs):
-    '''Removes indexes of atoms in transmute list with repeated electrostatic potential differences'''
+def remove_duplicate_espdiffs(dexlist, espdiffs, tol=0.01):
+    """
+    Filters and removes duplicate electrostatic potential differences from atoms
+    supplied in dexlist.
+
+    This functions first finds all values in espdiffs that are unique. Then the function
+    removes espdiffs that differ from others by the tol.
+
+    Parameters
+    ----------
+    dexlist : List of indexes of atoms with electrostatic potential differences to filter.
+
+    espdiffs : List of all electrostatic potential differences for the desired system
+        (see `espdiff()`).
+
+    tol (default: 0.01): Tolerance used for removing similar values from unique
+        electrostatic potential differences.
+
+    Returns
+    -------
+    unique_dexes : List of atom indexes that match the filtered electrostatic potential
+        differences.
+    """
+
     #Pairing espdiff values with corresponding atom index in dexlist
-    tol = 0.01
     espd_dict = {}
+
     for i in dexlist:
+
         espd_dict[str(i)] = espdiffs[i]
+
     #Grabbing unique espdiff values
     unique_diffs = {}
-    for dex,diff in espd_dict.items():
-        if diff not in unique_diffs.values():
-            unique_diffs[dex] = diff
-    print('Unique Electrostatic Potential Differences:')
-    print(unique_diffs.values())
-    print('')
 
+    for dex, diff in espd_dict.items():
+
+        if diff not in unique_diffs.values():
+
+            unique_diffs[dex] = diff
+
+    #print('Unique Electrostatic Potential Differences:')
+    #print(unique_diffs.values())
+    #print('')
+
+    #Filtering out espdiffs that are differ to others within tol
     copy_unique_diffs = unique_diffs.copy()
-    for dex,diff in copy_unique_diffs.items():
-        c = 0
-        for dex2,diff2 in unique_diffs.items():
+
+    for dex, diff in copy_unique_diffs.items():
+
+        counter = 0
+
+        for diff2 in unique_diffs.values():
+
             if abs(diff2 - diff) < tol and abs(diff2 - diff) != 0:
-                c += 1
-        if c > 0:
+
+                counter += 1
+
+        if counter > 0:
+
             del unique_diffs[dex]
-    
-    print('Unique Electrostatic Potential Differences WITHIN A TOLERANCE VALUE:')
-    unique_vals = [u for u in unique_diffs.values()]
-    print(unique_vals)
-    print(' ')
+
+    #print('Unique Electrostatic Potential Differences WITHIN A TOLERANCE VALUE:')
+    #unique_vals = [u for u in unique_diffs.values()]
+    #print(unique_vals)
+    #print(' ')
+
     unique_dexes = []
-    for dex in unique_diffs.keys():
+
+    for dex in unique_diffs:
+
         unique_dexes.append(int(dex))
-    
+
     return unique_dexes
 
-def heatmap(poscar,dexlist,espdiffs):
-    '''Assigns electrostatic potential differences to each atom in system as an initial charge in ASE GUI to visualize.'''
-    system = read(poscar,format='vasp')
-    system.set_initial_charges(espdiffs)
-    return system
-        
+def heatmap(slab, espdiffs):
+    """
+    Assigns electrostatic potential differences to each atom in slab as an
+    initial charge in ASE GUI to visualize.
+
+    Parameters
+    ----------
+    slab : An atoms object from ASE.
+
+    espdiffs : List of all electrostatic potential differences for slab (see `espdiff()`).
+
+    Returns
+    -------
+    slab : An atoms object from ASE with updated initial charges (slab.get_initial_charges()).
+    """
+
+    slab.set_initial_charges(espdiffs)
+
+    return slab
