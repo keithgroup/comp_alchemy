be603e12db3a1b6594dd317effd3dba20f38300b
Chasz Griego <charles.griego18@gmail.com>
2020-01-23 11:14:32 -0500

    added tests for index_transmuted.py

--- phystone/tests/test_phystone.py
+++ phystone/tests/test_phystone.py
@@ -12,23 +12,61 @@ def test_phystone_imported():
     assert "phystone" in sys.modules
 
 #Tests for transmutations.py
-from ase import Atom, Atoms
-from ase.build import fcc111, add_adsorbate
+from ase import Atom
+from ase.build import fcc111
 from ase.visualize import view
 
 slab = fcc111('Cu', size=(2, 2, 4), vacuum=10.0)
-transmute, counter = phystone.transmutations.index_transmuted(slab, 'Cu', 'Cu',
-                                                              8, 1)
+#view(slab)
+
+symmetric_slab_odd = fcc111('Cu', size=(2, 2, 9), vacuum=10.0, orthogonal=True)
+#view(symmetric_slab_odd)
+symmetric_slab_even = fcc111('Cu', size=(2, 2, 8), vacuum=10.0, orthogonal=True)
+#view(symmetric_slab_even)
+
 new_atom = Atom('Zn')
-transmuted_slab = phystone.transmutations.transmuter(slab, transmute,
+
+def test_index_transmuted():
+
+    expected_transmute = [12, 13, 14, 15, 8, 9, 10, 11]
+
+    expected_counter = [0, 1, 2, 3]
+
+    transmute, counter = phystone.transmutations.index_transmuted(slab,
+                                                                  'Cu',
+                                                                  'Cu', 8, 4)
+
+    assert (expected_transmute == transmute and
+            expected_counter == counter)
+
+
+def test_index_transmuted_with_symmetric():
+
+    expected_even_transmute = [28, 29, 30, 31, 24, 25, 26, 27, 0, 1, 2, 3, 4, 5, 6, 7]
+
+    expected_even_counter = [12, 13, 14, 15, 16, 17, 18, 19]
+
+    even_symmetric_transmute, even_symmetric_counter = phystone.transmutations.index_transmuted(
+        symmetric_slab_even,'Cu', 'Cu', 8, 4, symmetric=True)
+
+    assert (expected_even_transmute == even_symmetric_transmute and
+            expected_even_counter == even_symmetric_counter)
+
+    expected_odd_transmute = [32, 33, 34, 35, 28, 29, 30, 31, 0, 1, 2, 3, 4, 5, 6, 7]
+
+    expected_odd_counter = [16, 17, 18, 19]
+
+    odd_symmetric_transmute, odd_symmetric_counter = phystone.transmutations.index_transmuted(
+        symmetric_slab_odd,'Cu', 'Cu', 8, 4, symmetric=True)
+
+    assert (expected_odd_transmute == odd_symmetric_transmute and
+            expected_odd_counter == odd_symmetric_counter)
+
+def test_transmuter():
+
+    transmuted_slab = phystone.transmutations.transmuter(slab, transmute,
                                                      [new_atom]*len(transmute))
 
-symmetric_slab = fcc111('Cu', size=(2, 2, 9), vacuum=10.0, orthogonal=True)
-symmetric_transmute, symmetric_counter = phystone.transmutations.index_transmuted(
-    symmetric_slab,'Cu', 'Cu', 8, 1, symmetric=True)
-print(symmetric_slab.get_center_of_mass())
-print(symmetric_slab[17].position[2])
-from numpy import isclose
-print(isclose([symmetric_slab[17].position[2]],[symmetric_slab.get_center_of_mass()[2]]))
-#view(symmetric_slab)
-print(symmetric_transmute, symmetric_counter)
+def test_transmuter_with_symmetric():
+
+    pass
