0c8770cdcceb4f233ad71107388953742120c178
Chasz Griego <charles.griego18@gmail.com>
2020-02-13 09:45:26 -0500

    added read_vasp_energies to benchmark.py

--- phystone/benchmark.py
+++ phystone/benchmark.py
@@ -20,27 +20,20 @@ def setup_vasp_calcs(Alchemy, alc_data, **kwargs):
         calc = Vasp2(directory=transmute_ads_dir,**kwargs)
         calc.write_input(row['ads atoms object'])
 
-#TESTS
+def read_vasp_energies(Alchemy, alc_data):
 
-from ase import Atom
-from ase.io import read
-from ase.visualize import view
+    for index, row in alc_data.iterrows():
 
-slab_dir = 'tests/vasp_files/slab/'
-ads_dir = 'tests/vasp_files/ads/'
+        transmute_slab_dir = Alchemy.slab_dir + (f"{row['delta nuclear charge']}_deltaZ_" +
+                                                 f"{len(row['transmute indexes'])}_Nt/" +
+                                                 f"{row['label']}/")
 
-alc = Alchemy(slab_dir, ads_dir)
+        transmute_ads_dir = Alchemy.ads_dir + (f"{row['delta nuclear charge']}_deltaZ_" +
+                                               f"{len(row['transmute indexes'])}_Nt/" +
+                                               f"{row['label']}/")
 
-alc_data = alc.do_alchemy(1, 2, Atom('Pt'), Atom('Pt'), 4, 4)
+        calc = Vasp2(directory=transmute_slab_dir)
+        calc.read_energy()
 
-setup_vasp_calcs(alc, alc_data, kpts=(5,5,5), encut=350, nsw=500)
-
-#view(read('tests/vasp_files/slab/1_deltaZ_2_Nt/0.1.Au12.Au14.Ir0.Ir1/POSCAR'))
-
-#view(read('tests/vasp_files/ads/1_deltaZ_2_Nt/0.1.Au12.Au14.Ir0.Ir1/POSCAR'))
-
-#calc = test_vasp(directory='tests/vasp_files/',kpts=(5,5,5),encut=350,nsw=500)
-
-#print(calc)
-
-#calc.write_input(alc_data['slab atoms object'][0])
+        calc = Vasp2(directory=transmute_ads_dir)
+        calc.read_energy()
